d6a974cd0cfc70bd3d488ceb62450aa2228a9e4f
Ronaldo Barrera <onie_barrera@hotmail.com>
2019-02-07 12:22:06 -0500

    initial commit with requirements.txt

---
+++ api.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 12:13:36 2019
+
+@author: Onie
+"""
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+from requests import get, post
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class ProxyAPI(Resource):
+    def get(self, target_url):
+        return get(target_url).json()
+
+    def post(self, target_url):
+        headers = {'User-Agent': request.headers['User-Agent']}
+        data = request.form
+        response = post(target_url, data=data, headers=headers)
+        print(response.text)
+        return response.status_code
+
+
+api.add_resource(ProxyAPI, '/proxy/<path:target_url>', endpoint = 'proxy_ep')
+
+if __name__ == '__main__':
+    app.run(debug=True)
